dfd986cbd9a6c4a252493ab47f161918dce40475
mitsu0428 <mitsuprograming@gmail.com>
2023-03-06 20:29:12 +0900

    プロフィール画像変更

--- src/pages/components/common/ImageContainer.tsx
+++ src/pages/components/common/ImageContainer.tsx
@@ -5,7 +5,7 @@ import styled from "styled-components";
 const ImageContainer = () => {
   return (
     <ImageComponent
-      src="/images/mitsuhirookada.png"
+      src="/images/mitsuhiro-okada.jpg"
       alt="Mitsuhiro Okada"
       width={300}
       height={300}
@@ -17,5 +17,6 @@ export default ImageContainer;
 
 const ImageComponent = styled(Image)`
   border-radius: 50%;
-  border: 1px solid #ccc;
+  border: 1px solid #fff;
+  box-shadow: 0 0 7px #932e40, 0 0 10px #932e40, 0 0 21px #932e40;
 `;
