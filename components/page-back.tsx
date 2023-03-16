093f9b896d560794d3f670a405c30dde7b58b8d6
mitsu0428 <mitsuprograming@gmail.com>
2023-03-17 01:44:51 +0900

    formatと記事投入

--- components/page-back.tsx
+++ components/page-back.tsx
@@ -1,17 +1,17 @@
 import { CMS_NAME } from "../lib/constants";
 import Link from "next/link";
 
-const Intro = () => {
+const PageBack = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         結う -web
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        事業紹介 <Link href="/about">結う -web</Link> | {CMS_NAME}.
+        <Link href="/">Home | 結う -web</Link>
       </h4>
     </section>
   );
 };
 
-export default Intro;
+export default PageBack;
---
+++ pages/about.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Container from "../components/container";
+import PostTitle from "../components/post-title";
+import PageBack from "../components/page-back";
+
+function about() {
+  return (
+    <Container>
+      <PageBack />
+      <PostTitle>about</PostTitle>
+
+      <p className="text-lg leading-relaxed mb-4">【結う -webの紹介】</p>
+      <p className="text-lg leading-relaxed mb-4">
+        あなたとwebという2本の糸をわたしという1本の糸で結う。
+      </p>
+      <p className="text-lg leading-relaxed mb-4">
+        個人に寄り添うweb brandingのパートナーです。
+      </p>
+      <p className="text-lg leading-relaxed mb-4">三人寄れば文殊の知恵</p>
+      <p className="text-lg leading-relaxed mb-4">
+        3人で考えれば知恵の象徴の文殊菩薩にも勝るようなアイデアが生まれると言う言葉
+      </p>
+      <p className="text-lg leading-relaxed mb-4">
+        自分の好きな言葉の一つです。
+      </p>
+      <p className="text-lg leading-relaxed mb-4">
+        あなたという糸とwebという糸をわたしという1本の糸で結う。
+      </p>
+      <p className="text-lg leading-relaxed mb-4">
+        3本に結われた糸はこれから進めていく事業を大きく強化していくと思います。
+      </p>
+      <p className="text-lg leading-relaxed mb-4">
+        世の中、デジタル化やDXが叫ばれる中、
+      </p>
+      <p className="text-lg leading-relaxed mb-4">
+        親身になって伴走してくれる人を探すのは簡単ではありません。
+      </p>
+      <p className="text-lg leading-relaxed mb-4">
+        ・一人一人が個人として持つ思いを大切にしながらコミニュケーションを大切にし、
+      </p>
+      <p className="text-lg leading-relaxed mb-4">
+        ・それをwebの世界で形にする
+      </p>
+      <p className="text-lg leading-relaxed mb-4">
+        ・そして、DX/フロントエンドエンジニアとして持つスキルで
+      </p>
+      <p className="text-lg leading-relaxed mb-4">
+        あなたの事業を全力でサポートさせてください！！
+      </p>
+    </Container>
+  );
+}
+
+export default about;
