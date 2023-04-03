c0f9710f6ef3a2e3ad8864932ae0166c42b85884
mitsu0428 <mitsuprograming@gmail.com>
2023-04-04 00:02:51 +0900

    feat: google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import { AppProps } from 'next/app'
-import '../styles/index.css'
+import { AppProps } from "next/app";
+import "../styles/index.css";
+import GoogleTagManager from "../lib/GoogleTagManager";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GoogleTagManager googleTagManagerId="GTM-WGSD52Q" />
+      <Component {...pageProps} />
+    </>
+  );
 }
