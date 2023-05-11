b6676caaff5a495c44cdd3268b901fd9136bfde2
mitsu0428 <mitsuprograming@gmail.com>
2023-05-11 16:03:56 +0900

    feat: loading animation を改善

--- src/pages/components/page/Loading.tsx
+++ src/pages/components/page/Loading.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Loading: React.FC = () => {
   const [progress, setProgress] = React.useState(0);
@@ -20,43 +20,64 @@ const Loading: React.FC = () => {
   }, []);
 
   return (
-    <>
-      <ProgressBarContainer>
-        <ProgressBar progress={progress}>
-          <ProgressText>{`${progress}%`}</ProgressText>
-        </ProgressBar>
-      </ProgressBarContainer>
-    </>
+    <LoadingWrapper>
+      <LoadingContent>
+        <LoadingSpinner>
+          <LoaderCircle />
+        </LoadingSpinner>
+        <LoadingText>Loading... {progress}%</LoadingText>
+      </LoadingContent>
+    </LoadingWrapper>
   );
 };
 
 export default Loading;
 
-const ProgressBarContainer = styled.div`
+const LoadingWrapper = styled.div`
   position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 300px;
-  height: 5px;
-  border-radius: 8px;
-`;
-
-const ProgressBar = styled.div<{ progress: number }>`
-  width: ${({ progress }) => progress}%;
+  width: 100%;
   height: 100%;
-  box-shadow: 0 0 7px #fff;
-  transition: width 0.1s linear;
-  position: relative;
-  border-radius: 8px;
+  left: 0;
+  top: 0;
 `;
 
-const ProgressText = styled.div`
+const LoadingContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+`;
+
+const LoadingSpinner = styled.div`
+  width: 60px;
+  height: 60px;
+  position: relative;
+`;
+
+const spin = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const LoaderCircle = styled.div`
+  display: block;
   position: absolute;
-  top: -15px;
-  left: 0;
-  right: 0;
-  text-align: center;
-  color: #fff;
-  text-shadow: 0 0 7px #fff;
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  border: 1px solid transparent;
+  border-top-color: #efeffa;
+  animation: ${spin} 1s linear infinite;
+`;
+
+const LoadingText = styled.p`
+  color: #999;
+  font-size: 20px;
+  font-family: "PT Sans Narrow", sans-serif;
+  margin-top: 20px;
 `;
