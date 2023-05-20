9ec1e55532a8d8ff20356468f44aefbcb7e5d77c
mitsu0428 <mitsuprograming@gmail.com>
2023-05-21 00:23:28 +0900

    fix: favicon

--- src/pages/components/libs/SeoSettings.tsx
+++ src/pages/components/libs/SeoSettings.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable react/react-in-jsx-scope */
+import React from "react";
 import Head from "next/head";
 
 interface MetaData {
@@ -36,16 +36,80 @@ const SeoSettings = ({
         name="viewport"
         content="width=device-width,initial-scale=1.0,user-scalable=no"
       />
-      <meta name="description" content={description} />
-      <meta property="og:url" content={url} />
-      <meta property="og:title" content={title} />
-      <meta property="og:site_name" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:type" content="website" />
-      <meta property="og:image" content={imgUrl} />
-      <meta property="og:image:width" content={String(imgWidth)} />
-      <meta property="og:image:height" content={String(imgHeight)} />
-      <link rel="canonical" href={url} />
+      <meta
+        name="description"
+        content={description}
+      />
+      <meta
+        property="og:url"
+        content={url}
+      />
+      <meta
+        property="og:title"
+        content={title}
+      />
+      <meta
+        property="og:site_name"
+        content={title}
+      />
+      <meta
+        property="og:description"
+        content={description}
+      />
+      <meta
+        property="og:type"
+        content="website"
+      />
+      <meta
+        property="og:image"
+        content={imgUrl}
+      />
+      <meta
+        property="og:image:width"
+        content={String(imgWidth)}
+      />
+      <meta
+        property="og:image:height"
+        content={String(imgHeight)}
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicon/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon/favicon-16x16.png"
+      />
+      <link
+        rel="manifest"
+        href="/favicon/site.webmanifest"
+      />
+      <link
+        rel="mask-icon"
+        href="/favicon/safari-pinned-tab.svg"
+        color="#5bbad5"
+      />
+      <meta
+        name="msapplication-TileColor"
+        content="#da532c"
+      />
+      <meta
+        name="theme-color"
+        content="#ffffff"
+      />
+      <link
+        rel="canonical"
+        href={url}
+      />
     </Head>
   );
 };
