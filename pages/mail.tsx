a49ba297ca3cd2f06871cb7d5fd9e4b97a7a6efe
mitsu0428 <mitsuprograming@gmail.com>
2023-05-20 22:34:22 +0900

    fix: contact フォームが壊れていた

--- pages/mail.tsx
+++ pages/mail.tsx
@@ -7,84 +7,103 @@ export default function Mail() {
   const [mail, setMail] = useState("");
   const [message, setMessage] = useState("");
 
-  // 指定秒を待機する関数
-  const _sleep = (ms: number) =>
-    new Promise((resolve) => setTimeout(resolve, ms));
-  // メール送信関数
+  const _sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
   const sendMail = async () => {
-    // 名前が入力されているか
-    if (name == "") {
-      alert("名前を入力してください！");
+    if (name === "") {
+      alert("名前を入力してください。");
       return;
     }
-    // メールアドレスが入力されているかと正しいフォーマットか
-    if (mail == "") {
+    if (mail === "") {
       alert("メールアドレスを入力してください。");
       return;
-    } else if (mail.indexOf("@") == -1) {
+    } else if (mail.indexOf("@") === -1) {
       alert("有効なメールアドレスを入力してください。");
       return;
     }
-    // 内容が入力されているか
-    if (message == "") {
+    if (message === "") {
       alert("内容を入力してください。");
       return;
     }
+
     await _sleep(1000);
+
     alert("お問い合わせを送信しました。");
+
     await fetch("/api/mail", {
       method: "POST",
-      body: `\n
-      名前: ${name} \n
-      メールアドレス: ${mail} \n
-      お問い合わせ内容: ${message} `,
+      body: `
+        名前: ${name}
+        メールアドレス: ${mail}
+        お問い合わせ内容: ${message}
+      `,
     });
-    // 値は保持されているのでリロードしてOK
+
     location.reload();
   };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+      <div className="w-96">
         <h1 className="text-6xl font-bold">お問い合わせ</h1>
-        <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-          <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-            <h2 className="text-2xl font-bold">名前</h2>
-            <input
-              className="w-1/2 h-10 px-3 mb-10 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline"
-              placeholder="名前"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
-          <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-            <h2 className="text-2xl font-bold">メールアドレス</h2>
-            <input
-              className="w-1/2 h-10 px-3 mb-10 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline"
-              placeholder="メールアドレス"
-              value={mail}
-              onChange={(e) => setMail(e.target.value)}
-            />
-          </div>
-          <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-            <h2 className="text-2xl font-bold">お問い合わせ内容</h2>
-            <textarea
-              className="w-1/2 h-40 px-3 mb-10 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline"
-              placeholder="お問い合わせ内容"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            />
-          </div>
-          <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-            <button
-              className="w-1/2 h-10 px-3 mb-10 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline"
-              onClick={sendMail}
-            >
-              送信
-            </button>
-          </div>
+        <div className="flex flex-col items-start mt-8">
+          <label
+            className="text-2xl font-bold"
+            htmlFor="name"
+          >
+            名前
+          </label>
+          <input
+            className="w-full h-10 px-3 mt-2 text-base placeholder-gray-600 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+            id="name"
+            type="text"
+            placeholder="名前"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
-        <Link href="/">ホームに戻る</Link>
+        <div className="flex flex-col items-start mt-8">
+          <label
+            className="text-2xl font-bold"
+            htmlFor="email"
+          >
+            メールアドレス
+          </label>
+          <input
+            className="w-full h-10 px-3 mt-2 text-base placeholder-gray-600 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+            id="email"
+            type="email"
+            placeholder="メールアドレス"
+            value={mail}
+            onChange={(e) => setMail(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col items-start mt-8">
+          <label
+            className="text-2xl font-bold"
+            htmlFor="message"
+          >
+            お問い合わせ内容
+          </label>
+          <textarea
+            className="w-full h-40 px-3 mt-2 text-base placeholder-gray-600 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+            id="message"
+            placeholder="お問い合わせ内容"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </div>
+        <div className="mt-8">
+          <button
+            className="w-full h-10 px-3 text-base text-white bg-blue-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+            onClick={sendMail}
+          >
+            送信
+          </button>
+        </div>
+        <div className="mt-8">
+          <Link href="/">ホームに戻る</Link>
+        </div>
       </div>
     </div>
   );
