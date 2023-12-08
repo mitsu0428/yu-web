8ed6810073a286e59c0a38aaa7f7791513290d54
mitsu0428 <mitsuprograming@gmail.com>
2023-12-08 15:42:07 +0900

    fix: 実績の追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import * as Footer from "../components/layout/Footer";
 import * as TitleWrapper from "../components/ui/TitleWrapper";
 import * as CenterText from "../components/ui/Text/CenterText";
 import * as Menu from "../components/ui/Menu";
+import * as Achievements from "../components/ui/Achievements";
 
 import Button from "../components/ui/Button";
 
@@ -21,14 +22,24 @@ const Home = () => {
         subtitle="# Front-End & Web"
         explainText="Web制作, LP制作, フロントエンド開発"
       />
-      <CenterText.Component text="Web/LP制作" />
+      <CenterText.Component text="Web/LP制作 領域" />
       <Hr />
 
       <TitleWrapper.Component
         subtitle="# Branding"
         explainText="ブランディング支援, デザイン, 撮影"
       />
-      <CenterText.Component text="撮影/レタッチ" />
+      <CenterText.Component text="撮影/レタッチ 領域" />
+      <Achievements.Component
+        title="bar kaze -円山町"
+        explainText="SK Building 4F, 1-3 Maruyamacho, Shibuya-ku"
+        imagePath="/achievements/barkaze.jpg"
+      />
+      <Achievements.Component
+        title="茶々-ChaCha TOKYO-"
+        explainText="西原2-28-4 102, Shibuya, Tokyo 151-0066"
+        imagePath="/achievements/chachatokyo.jpg"
+      />
 
       <Hr />
       <Footer.Component />
