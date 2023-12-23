720214e657893622f4f17f9cbed18bf2125a5081
mitsu0428 <mitsuprograming@gmail.com>
2023-12-23 19:53:48 +0900

    fix: yu-logo

--- src/components/features/YuTopPage.tsx
+++ src/components/features/YuTopPage.tsx
@@ -8,41 +8,62 @@ export const Component = () => {
     `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`;
   const images = [
     // Front
-    { position: [0, 0, 1.5], rotation: [0, 0, 0], url: pexel(1103970) },
+    {
+      position: [0, 0, 1.5],
+      rotation: [0, 0, 0],
+      // url: pexel(1103970),
+      url: "/yu-logo/logo-tate.png",
+    },
     // Back
-    { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(416430) },
-    { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(310452) },
+    {
+      position: [-0.8, 0, -0.6],
+      rotation: [0, 0, 0],
+      // url: pexel(416430),
+      url: "/yu-logo/logo-tate.png",
+    },
+    {
+      position: [0.8, 0, -0.6],
+      rotation: [0, 0, 0],
+      // url: pexel(310452),
+      url: "/yu-logo/logo-tate.png",
+    },
     // Left
     {
       position: [-1.75, 0, 0.25],
       rotation: [0, Math.PI / 2.5, 0],
-      url: pexel(327482),
+      // url: pexel(327482),
+      url: "/yu-logo/logo-tate.png",
     },
     {
       position: [-2.15, 0, 1.5],
       rotation: [0, Math.PI / 2.5, 0],
-      url: pexel(325185),
+      // url: pexel(325185),
+      url: "/yu-logo/logo-tate.png",
     },
     {
       position: [-2, 0, 2.75],
       rotation: [0, Math.PI / 2.5, 0],
-      url: pexel(358574),
+      // url: pexel(358574),
+      url: "/yu-logo/logo-tate.png",
     },
     // Right
     {
       position: [1.75, 0, 0.25],
       rotation: [0, -Math.PI / 2.5, 0],
-      url: pexel(227675),
+      // url: pexel(227675),
+      url: "/yu-logo/logo-tate.png",
     },
     {
       position: [2.15, 0, 1.5],
       rotation: [0, -Math.PI / 2.5, 0],
-      url: pexel(911738),
+      // url: pexel(911738),
+      url: "/yu-logo/logo-tate.png",
     },
     {
       position: [2, 0, 2.75],
       rotation: [0, -Math.PI / 2.5, 0],
-      url: pexel(1738986),
+      // url: pexel(1738986),
+      url: "/yu-logo/logo-tate.png",
     },
   ];
   return (
