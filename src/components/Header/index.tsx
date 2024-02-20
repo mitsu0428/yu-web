a6c1e925c98017c2affe1072c9db4c4adbc8e0cc
mitsu0428 <mitsuprograming@gmail.com>
2024-02-21 00:46:10 +0900

    fix: 不要なものを削除

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
-import * as Logo from "../../ui/Logo";
+import * as Logo from "../Logo";
 import font from "../../styles/NextFont";
 
 export const Component = () => {
