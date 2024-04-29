4d64ebb5defe0845d37e0b2b57a3e7248d2a0fcd
mitsu0428 <mitsuprograming@gmail.com>
2024-04-29 14:35:58 +0900

    fix: test vanilla css

--- yu-web-front/src/components/About/index.tsx
+++ yu-web-front/src/components/About/index.tsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import { container } from "../../style/Home.css";
 
 export const Component = () => {
   return (
-    <div>
+    <div className={container}>
+      <h2>コンセプト</h2>
+
       <Link to="/">Home</Link>
-      <div>About</div>
     </div>
   );
 };
--- yu-web-front/src/components/Home/index.tsx
+++ yu-web-front/src/components/Home/index.tsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import { container } from "../../style/Home.css";
 
 export const Component = () => {
   return (
-    <div>
-      <div>Home</div>
+    <div className={container}>
+      <h2>結う -web</h2>
+
       <Link to="/about">About</Link>
     </div>
   );
